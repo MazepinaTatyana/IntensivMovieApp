05d9ed262669569e7f55c83cfdcea3cf7536424f
Мазепина Татьяна <t.mazepina@mediasoft.team>
2021-07-16 09:56:03 +0300

    added coroutines to TvShowsFragment.kt

--- app/src/main/java/ru/androidschool/intensiv/presentation/tvshows/TvShowPresenter.kt
+++ app/src/main/java/ru/androidschool/intensiv/presentation/tvshows/TvShowPresenter.kt
@@ -1,22 +1,27 @@
 package ru.androidschool.intensiv.presentation.tvshows
 
 import android.annotation.SuppressLint
+import kotlinx.coroutines.coroutineScope
+import ru.androidschool.intensiv.data.vo.Movie
 import ru.androidschool.intensiv.domain.usecase.remote_use_case.TvShowsRemoteUseCase
 import ru.androidschool.intensiv.presentation.base.BasePresenter
 import timber.log.Timber
 
 class TvShowPresenter(private val useCase: TvShowsRemoteUseCase) : BasePresenter<TvShowView>() {
 
+    private var tvShows = listOf<Movie>()
+
     @SuppressLint("TimberArgCount")
-    fun getTvShows() {
-        val disposable = useCase.getTVShows()
-            .doOnSubscribe { view?.showProgressBar() }
-            .doFinally { view?.hideProgressBar() }
-            .subscribe({
-                view?.showTvShows(it)
-            }, { error ->
+    suspend fun getTvShows() {
+        view?.showProgressBar()
+        coroutineScope {
+            try {
+                tvShows = useCase.getTVShows()
+                view?.showTvShows(tvShows)
+                view?.hideProgressBar()
+            } catch (error: Exception) {
                 Timber.d("Error tvShow", error.message)
-            })
-        compositeDisposable.add(disposable)
+            }
+        }
     }
 }
