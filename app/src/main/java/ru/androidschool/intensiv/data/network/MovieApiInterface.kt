0efeb7ad4419bb96793eac4eafa6c9525a7a8704
Мазепина Татьяна <t.mazepina@mediasoft.team>
2021-07-16 09:39:40 +0300

    changed tvshows repositories

--- app/src/main/java/ru/androidschool/intensiv/data/network/MovieApiInterface.kt
+++ app/src/main/java/ru/androidschool/intensiv/data/network/MovieApiInterface.kt
@@ -1,6 +1,7 @@
 package ru.androidschool.intensiv.data.network
 
 import io.reactivex.Single
+import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
